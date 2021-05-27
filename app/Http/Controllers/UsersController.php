5972acb652dc35aa01da3e4b35c48a8ab2ffe401
Captoweb <captainphp@yandex.ru>
2021-05-27 22:34:33 +0300

    store and update actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,11 +34,13 @@ class UsersController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        //сохранение в бд
+        User::create($request->only(['name', 'email']));
+        return redirect()->route('users.index');
     }
 
     /**
@@ -73,7 +75,8 @@ class UsersController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user->update($request->only(['name', 'email']));
+        return redirect()->route('users.index');
     }
 
     /**
